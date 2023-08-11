d561d29bfb39e4bfb8e34d32953f6d7b431db761
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-12 03:40:01 +0500

    practive

---
+++ lib/practice.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PracticeWidgets extends StatelessWidget {
+  const PracticeWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
