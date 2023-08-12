20ef7aeb81b2a8a0718f0f66213dd59767e757f8
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-12 16:30:24 +0500

    practice

--- lib/practice.dart
+++ lib/practice.dart
@@ -15,173 +15,181 @@ class PracticeWidgets extends StatelessWidget {
         children: [
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade500
-                      ),
-                      const BoxShadow(
-                        blurRadius: 15,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade400
-                      ),
-                      const BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+                  const SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade400
-                      ),
-                      const BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+                  const SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade400
-                      ),
-                      const BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+                  const SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade400
-                      ),
-                      const BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+                  const SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: const Offset(4, 4),
-                        color: Colors.grey.shade400
-                      ),
-                      const BoxShadow(
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(-4, -4),
-                        color: Colors.white
-                      ),
-                    ]
+                  const SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500
+                        ),
+                        const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white
+                        ),
+                      ]
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: const Center(
+                        child: Text(
+                      'Design',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )),
                   ),
-                  height: 100,
-                  width: 100,
-                  child: const Center(
-                      child: Text(
-                    'Design',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                  )),
-                ),
-              ],
+                ],
+              ),
             ),
           )
         ],
