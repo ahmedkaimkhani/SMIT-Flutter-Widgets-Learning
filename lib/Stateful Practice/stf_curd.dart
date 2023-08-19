e7621029b2a24a26678bc393d7ee679a796ed7fd
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-20 02:52:30 +0500

    ui

--- lib/Stateful Practice/stf_curd.dart
+++ lib/Stateful Practice/stf_curd.dart
@@ -11,18 +11,35 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey[300],
         body: SafeArea(
-      child: Column(
-        children: [
-          Row(
-            children: const [
-              Text('Curd Operations'),
-              Icon(Icons.add),
-              Icon(Icons.restore_from_trash_sharp)
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(width: 0.5, color: Colors.grey)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: const [
+                      Text(
+                        'Curd Operations',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(Icons.add),
+                      Icon(Icons.restore_from_trash_sharp)
+                    ],
+                  ),
+                ),
+              )
             ],
-          )
-        ],
-      ),
-    ));
+          ),
+        ));
   }
 }
