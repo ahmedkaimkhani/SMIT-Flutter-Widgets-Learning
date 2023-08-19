750d2fa478d4592f926ca43a2830ee44ff181e92
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-20 02:29:17 +0500

    todo

--- lib/Stateful Practice/stf_curd.dart
+++ lib/Stateful Practice/stf_curd.dart
@@ -12,9 +12,11 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const Icon(Icons.menu),
         backgroundColor: Colors.deepPurple[400],
         title: const Text('Curd Operations'),
         centerTitle: true,
+        actions: const [Icon(Icons.restore_from_trash_sharp)],
       ),
       body: Container(
         color: Colors.yellow,
