7e81863dc86c069053b5f516f427449fc10dc71e
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-19 15:29:55 +0500

    ui

---
+++ lib/Stateful Practice/stf_curd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StfCurdPractice extends StatefulWidget {
+  const StfCurdPractice({super.key});
+
+  @override
+  State<StfCurdPractice> createState() => _StfCurdPracticeState();
+}
+
+class _StfCurdPracticeState extends State<StfCurdPractice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
