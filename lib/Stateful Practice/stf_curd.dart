0c907f89ca101874c2b1a7eed420577932b6259c
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-20 04:41:34 +0500

    ui

--- lib/Stateful Practice/stf_curd.dart
+++ lib/Stateful Practice/stf_curd.dart
@@ -51,6 +51,39 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
                   ),
                 ),
               ),
+              Container(
+                margin: const EdgeInsets.only(top: 25, left: 15, right: 15),
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                    // border: Border.all(width: 0.5, color: Colors.grey),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500),
+                      const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    cursorColor: Colors.deepPurple,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        suffixIcon: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.add,
+                              color: Colors.deepPurple,
+                            ))),
+                  ),
+                ),
+              ),
             ],
           ),
         ));
