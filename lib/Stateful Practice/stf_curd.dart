6a0d08604b005326e94ec7edab60140889488520
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-21 01:55:44 +0500

    ui

--- lib/Stateful Practice/stf_curd.dart
+++ lib/Stateful Practice/stf_curd.dart
@@ -8,92 +8,29 @@ class StfCurdPractice extends StatefulWidget {
 }
 
 class _StfCurdPracticeState extends State<StfCurdPractice> {
+  List friensList = [
+    'Ahmed',
+    'Usman',
+    'Bazil',
+    'Asim',
+    'Naveed',
+    'Bilal',
+    'Hamza',
+    'Babu'
+  ];
+
   @override
   Widget build(BuildContext context) {
+    friensList.addAll(['Hassam, lala, Khan']);
+
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(8),
-                // border: Border.all(width: 0.5, color: Colors.grey),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 15,
-                      spreadRadius: 1,
-                      offset: const Offset(4, 4),
-                      color: Colors.grey.shade500),
-                  const BoxShadow(
-                      blurRadius: 15,
-                      spreadRadius: 1,
-                      offset: Offset(-4, -4),
-                      color: Colors.white),
-                ]),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Row(
-                children: const [
-                  Text(
-                    'Curd Operations',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.deepPurple),
-                  ),
-                  Spacer(),
-                  Icon(Icons.restore_from_trash_sharp, color: Colors.deepPurple)
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 25, left: 15, right: 15),
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(5),
-                // border: Border.all(width: 0.5, color: Colors.grey),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 15,
-                      spreadRadius: 1,
-                      offset: const Offset(4, 4),
-                      color: Colors.grey.shade500),
-                  const BoxShadow(
-                      blurRadius: 15,
-                      spreadRadius: 1,
-                      offset: Offset(-4, -4),
-                      color: Colors.white),
-                ]),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                cursorColor: Colors.deepPurple,
-                decoration: InputDecoration(
-                    hintText: 'Enter content',
-                    hintStyle: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w800),
-                    // fillColor: Colors.grey,
-                    border: InputBorder.none,
-                    suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.add,
-                          color: Colors.deepPurple,
-                        ))),
-              ),
-            ),
-          ),
-          ListView.builder(
-            itemCount: 2,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            children: [
+              Container(
                 margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
                 decoration: BoxDecoration(
                     color: Colors.grey[300],
@@ -128,10 +65,92 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
                     ],
                   ),
                 ),
-              );
-            },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 25, left: 15, right: 15),
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5),
+                    // border: Border.all(width: 0.5, color: Colors.grey),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: const Offset(4, 4),
+                          color: Colors.grey.shade500),
+                      const BoxShadow(
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                          offset: Offset(-4, -4),
+                          color: Colors.white),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    cursorColor: Colors.deepPurple,
+                    decoration: InputDecoration(
+                        hintText: 'Enter content',
+                        hintStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w800),
+                        // fillColor: Colors.grey,
+                        border: InputBorder.none,
+                        suffixIcon: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.add,
+                              color: Colors.deepPurple,
+                            ))),
+                  ),
+                ),
+              ),
+              ListView.builder(
+                itemCount: friensList.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8),
+                        // border: Border.all(width: 0.5, color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: const Offset(4, 4),
+                              color: Colors.grey.shade500),
+                          const BoxShadow(
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(-4, -4),
+                              color: Colors.white),
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: [
+                          Text(
+                            friensList[index].toString(),
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.deepPurple),
+                          ),
+                          const Spacer(),
+                          const Icon(Icons.restore_from_trash_sharp,
+                              color: Colors.deepPurple)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
