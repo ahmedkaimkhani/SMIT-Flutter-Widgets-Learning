c30b4a728e55a8f6f024285eb76e698af95f478f
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-20 20:49:56 +0500

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_class_1/Counter/counter_increment.dart';
 import 'package:flutter_class_1/Stateful%20Practice/stf_curd.dart';
 import 'package:flutter_class_1/home_screen.dart';
 import 'package:flutter_class_1/practice.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StfCurdPractice(),
+      home: CounterIncrement(),
     );
   }
 }
