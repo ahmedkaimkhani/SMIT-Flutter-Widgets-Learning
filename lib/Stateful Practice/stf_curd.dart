9709b467baebbf9a93201600bd7c09076576f762
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-20 16:58:21 +0500

    ui

--- lib/Stateful Practice/stf_curd.dart
+++ lib/Stateful Practice/stf_curd.dart
@@ -75,8 +75,10 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
                     cursorColor: Colors.deepPurple,
                     decoration: InputDecoration(
                         hintText: 'Enter content',
-                        hintStyle:
-                            const TextStyle(fontSize: 14, color: Colors.grey),
+                        hintStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w800),
                         // fillColor: Colors.grey,
                         border: InputBorder.none,
                         suffixIcon: IconButton(
@@ -88,6 +90,48 @@ class _StfCurdPracticeState extends State<StfCurdPractice> {
                   ),
                 ),
               ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8),
+                        // border: Border.all(width: 0.5, color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: const Offset(4, 4),
+                              color: Colors.grey.shade500),
+                          const BoxShadow(
+                              blurRadius: 15,
+                              spreadRadius: 1,
+                              offset: Offset(-4, -4),
+                              color: Colors.white),
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: const [
+                          Text(
+                            'Curd Operations',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.deepPurple),
+                          ),
+                          Spacer(),
+                          Icon(Icons.restore_from_trash_sharp,
+                              color: Colors.deepPurple)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )
             ],
           ),
         ));
