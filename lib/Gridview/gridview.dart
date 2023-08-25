fa375ea3f1b92c5e593d293a4eda1a5b06942b8d
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-25 20:29:33 +0500

    gridview

--- lib/Gridview/gridview.dart
+++ lib/Gridview/gridview.dart
@@ -5,6 +5,20 @@ class MyGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
