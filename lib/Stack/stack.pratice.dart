34670bb46f4b6b4358bdfe55e9a1e1bfde4e454e
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-27 23:47:47 +0500

    stack practice

--- lib/Stack/stack.pratice.dart
+++ lib/Stack/stack.pratice.dart
@@ -6,29 +6,65 @@ class StackWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Stock Practice'),
-      ),
-      body: Center(
-        child: Stack(children: [
-          Container(
-            color: Colors.red,
-            height: 120,
-            width: 120,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Stock Practice'),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
           ),
-          Container(
-            color: Colors.yellow,
-            height: 100,
-            width: 100,
-          ),
-          Container(
-            color: Colors.green,
-            height: 80,
-            width: 80,
-          )
-        ]),
-      ),
-    );
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Stack(children: [
+                Center(
+                  child: Container(
+                    color: Colors.red,
+                    height: 120,
+                    width: 120,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.yellow,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.green,
+                    height: 80,
+                    width: 80,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.deepPurple,
+                    height: 60,
+                    width: 60,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.amber,
+                    height: 40,
+                    width: 40,
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.orange,
+                    height: 20,
+                    width: 20,
+                  ),
+                )
+              ]),
+            );
+          },
+        ));
   }
 }
