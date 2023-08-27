e953813e19ad21614d24cc0e906f948547df9685
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-28 04:43:09 +0500

    stack

---
+++ lib/Stack/stack_postioned.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StackPractice extends StatelessWidget {
+  const StackPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stack Practie'),
+      ),
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
