600ad68c1080e9c186c17942828cdb22f9c11040
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-07 02:02:02 +0500

    Grocery

--- lib/Async Function/async_practice.dart
+++ lib/Async Function/async_practice.dart
@@ -11,6 +11,19 @@ class _AsyncFunctionState extends State<AsyncFunction> {
   int counter = 0;
   int counter2 = 0;
   int counter3 = 0;
+
+  updateValue() {
+    counter++;
+    counter2++;
+    setState(() {});
+    counter3++;
+
+    // print statement is for checking counter is working perfectly or not.
+    print(counter);
+    print(counter2);
+    print(counter3);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +32,16 @@ class _AsyncFunctionState extends State<AsyncFunction> {
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(counter.toString()),
           Text(counter2.toString()),
           Text(counter3.toString()),
-          ElevatedButton(onPressed: () {}, child: const Text('Fetch')),
+          ElevatedButton(
+              onPressed: () {
+                updateValue();
+              },
+              child: const Text('Fetch')),
         ],
       ),
     );
