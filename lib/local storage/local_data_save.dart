d74e4d37aff07f399470e0410a17a2ea72729916
ahmedkaimkhani <ak9302359@gmail.com>
2023-09-10 17:51:53 +0500

    Practice

--- lib/local storage/local_data_save.dart
+++ lib/local storage/local_data_save.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorage extends StatefulWidget {
   const LocalStorage({super.key});
@@ -8,6 +9,34 @@ class LocalStorage extends StatefulWidget {
 }
 
 class _LocalStorageState extends State<LocalStorage> {
+  String isLogin = 'user not logged in';
+
+  setLogin() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isLogin = 'User logged in';
+    await prefs.setString('isLogin', isLogin);
+    setState(() {});
+  }
+
+  getLoginState() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isLogin = prefs.getString(
+          'isLogin',
+        ) ??
+        isLogin;
+    setState(() {});
+  }
+
+  fetchInitialData() async {
+    await getLoginState();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchInitialData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,14 +44,21 @@ class _LocalStorageState extends State<LocalStorage> {
         title: const Text('Local Storage'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const Text(
-            'text',
-            style: TextStyle(fontSize: 35),
-          ),
-          TextButton(onPressed: () {}, child: const Text('Login'))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              isLogin,
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setLogin();
+                },
+                child: const Text('Login'))
+          ],
+        ),
       ),
     );
   }
